ad28d39ca0520d465e48adfdbcdcd38f6a6221ce
fanGY <245379383@qq.com>
2021-04-21 23:01:56 +0800

    0421

--- src/main/java/twoPointers/LeetCode881.java
+++ src/main/java/twoPointers/LeetCode881.java
@@ -1,4 +1,23 @@
 package twoPointers;
 
+import java.util.Arrays;
+
 public class LeetCode881 {
+
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int res = 0;
+        int i = 0;
+        int j = people.length - 1;
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--;
+            res++;
+        }
+        return res;
+    }
+
+
 }
