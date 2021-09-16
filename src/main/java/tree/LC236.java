beee55d05464290416633c0e357039e610e7ee0d
fanGY <245379383@qq.com>
2021-09-16 23:35:48 +0800

    二叉树最近公共祖先

---
+++ src/main/java/tree/LC236.java
@@ -0,0 +1,25 @@
+package tree;
+
+/**
+ * 236. 二叉树的最近公共祖先
+ */
+public class LC236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left == null) {
+            return right;
+        }
+
+        return left;
+    }
+}
