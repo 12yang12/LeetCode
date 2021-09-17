271a975f44f29865200e26cfad9f106945cbf561
fanGY <245379383@qq.com>
2021-09-17 23:04:07 +0800

    删除二叉搜索树中的节点

---
+++ src/main/java/tree/LC450.java
@@ -0,0 +1,37 @@
+package tree;
+
+/**
+ * 450. 删除二叉搜索树中的节点
+ */
+public class LC450 {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                TreeNode cur = root.right;
+                while (cur.left != null) {
+                    cur = cur.left;
+                }
+                cur.left = root.left;
+                root = root.right;
+
+                return root;
+            }
+        }
+
+        return root;
+    }
+
+}
