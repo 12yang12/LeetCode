307f4fc0e02daf202cf3ba111a9df68291fcfd47
fanGY <245379383@qq.com>
2022-01-05 07:16:23 +0800

    509

---
+++ src/main/java/dynamicProgramming/LC509.java
@@ -0,0 +1,36 @@
+package dynamicProgramming;
+
+public class LC509 {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public int fib2(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        int[] dp = new int[2];
+        dp[0] = 0;
+        dp[1] = 1;
+        int sum = 0;
+        for (int i = 2; i <= n; i++) {
+            sum = dp[1] + dp[0];
+            dp[0]=dp[1];
+            dp[1] = sum;
+        }
+
+        return dp[1];
+    }
+}
