d413101d5fba27a09eb8ee1b133a2d833c69af8c
fanGY <245379383@qq.com>
2022-05-04 19:46:32 +0800

    496. 下一个更大元素 I

--- src/main/java/stack/LC496.java
+++ src/main/java/stack/LC496.java
@@ -1,31 +1,41 @@
 package stack;
 
+
 import java.util.*;
 
-public class LeetCode496 {
+/**
+ * 496. 下一个更大元素 I
+ * 给你两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。
+ *
+ * 请你找出 nums1 中每个元素在 nums2 中的下一个比其大的值。
+ * nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。
+ * 如果不存在，对应位置输出 -1 。
+ */
+public class LC496 {
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Deque<Integer> stack = new ArrayDeque<>();
         Map<Integer, Integer> map = new HashMap<>();
-        // 先处理 nums2,把对应关系存入哈希表
+
         for (int num : nums2) {
             while (!stack.isEmpty() && num > stack.peekLast()) {
                 map.put(stack.pollLast(), num);
             }
             stack.addLast(num);
         }
-        // 遍历 nums1 得到结果集
+
         int[] result = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
             result[i] = map.getOrDefault(nums1[i], -1);
         }
+
         return result;
     }
 
     public static void main(String[] args) {
         int[] nums1 = new int[]{4, 1, 2};
         int[] nums2 = new int[]{1, 3, 4, 2};
-        int[] result = new LeetCode496().nextGreaterElement(nums1, nums2);
+        int[] result = new LC496().nextGreaterElement(nums1, nums2);
         System.out.println(Arrays.toString(result));
 
     }
