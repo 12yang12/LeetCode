9418afc8bb61beeeb92875f31b8ee128050e5e28
fanGY <245379383@qq.com>
2022-07-22 07:42:44 +0800

    69. x 的平方根

--- src/main/java/binarySearch/LC704.java
+++ src/main/java/binarySearch/LC704.java
@@ -1,11 +1,16 @@
 package binarySearch;
 
-public class LeetCode704 {
+/**
+ * 给定一个n个元素有序的（升序）整型数组nums 和一个目标值target ，写一个函数搜索nums中的 target，
+ * 如果目标值存在返回下标，否则返回 -1。
+ */
+public class LC704 {
 
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
         int mid = left + (right - left) / 2;
+
         while (left <= right) {
             if (nums[mid] == target) {
                 return mid;
@@ -16,6 +21,7 @@ public class LeetCode704 {
             }
             mid = left + (right - left) / 2;
         }
+
         return -1;
     }
 }
