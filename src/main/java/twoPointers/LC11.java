bf39bd203c0143f24169b6da0ba9ddb9c1efe7da
fanGY <245379383@qq.com>
2022-08-05 06:31:47 +0800

    11. 盛最多水的容器

---
+++ src/main/java/twoPointers/LC11.java
@@ -0,0 +1,35 @@
+package twoPointers;
+
+/**
+ * 11. 盛最多水的容器
+ * 给定一个长度为 n 的整数数组height。有n条垂线，第 i 条线的两个端点是(i, 0)和(i, height[i])。
+ * 找出其中的两条线，使得它们与x轴共同构成的容器可以容纳最多的水。
+ * 返回容器可以储存的最大水量。
+ * 说明：你不能倾斜容器。
+ */
+public class LC11 {
+    // 暴力解法
+    public int maxArea1(int[] height) {
+        int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
+            }
+        }
+        return maxArea;
+    }
+
+    // 双指针
+    public int maxArea(int[] height) {
+        int maxArea = 0, left = 0, right = height.length - 1;
+        while (left < right) {
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
