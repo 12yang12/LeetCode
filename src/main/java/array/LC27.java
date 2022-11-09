ffa09233800e17ccb87166667aad3feb56626ad3
fanGY <245379383@qq.com>
2022-11-09 13:04:02 +0800

    renbame class

--- src/main/java/array/LC27.java
+++ src/main/java/array/LC27.java
@@ -7,12 +7,11 @@ import java.util.Arrays;
  * 给你一个数组 nums和一个值 val，你需要 原地 移除所有数值等于val的元素，并返回移除后数组的新长度。
  * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
  * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
- *
- * 链接：https://leetcode-cn.com/problems/remove-element
+ * <a href="https://leetcode-cn.com/problems/remove-element">...</a>
  * @author Yang
  */
 
-public class LeetCode27 {
+public class LC27 {
     /**
      * Two Pointers
      * Time Complexity: O(N)
@@ -46,7 +45,7 @@ public class LeetCode27 {
     public static void main(String[] args) {
         int[] arr = new int[] {0,1,2,2,3,0,4,2};
         System.out.println(Arrays.toString(arr));
-        int result = new LeetCode27().removeElement(arr, 2);
+        int result = new LC27().removeElement(arr, 2);
         System.out.println(result);
         System.out.println(Arrays.toString(arr));
     }
